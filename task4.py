f8ab2535e6d989ead3cbfe2d1fc135db66a75b61
oyankhan <oyankhan6969@gmail.com>
2024-07-06 22:14:23 +0600

    New File Added

---
+++ task4.py
@@ -0,0 +1,12 @@
+# X= [ 10,20,30,40,50,60]
+# for y in X:
+#     print(f"{y} * 2 = {y * 2}")
+
+## By using while loop
+Sum = 0
+counter = 1
+
+while counter <= 10 :
+    Sum = Sum + counter
+    counter = counter+1
+print("the series ", Sum )
